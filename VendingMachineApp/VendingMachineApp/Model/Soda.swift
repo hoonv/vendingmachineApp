43c13e7cc71167b3bc74daac9df5278b4927fd10
hoonv <chaeefe@gmail.com>
2020-09-14 22:41:18 +0900

    feat: apply protocols to Soda & children
    - 소다 클래스엔 칼로리를 체크할 수 있는 프로토콜 추가
    - 사이다, 콜라엔 설탕추카

--- VendingMachineApp/VendingMachineApp/Model/Soda.swift
+++ VendingMachineApp/VendingMachineApp/Model/Soda.swift
@@ -8,20 +8,48 @@
 
 import Foundation
 
-enum SodaTaste {
-    case coke
-    case cider
-    case fanta
+class Soda: Beverage, CalorieCheckable {
+    
+    internal let calorieContent: Int
+        
+    init(brand: String, capacity: Int, price: Int, name: String, date: Date, calorie: Int) {
+        calorieContent = calorie
+        super.init(brand: brand, capacity: capacity, price: price, name: name, date: date)
+    }
+    
+    init(beverage: Beverage, calorie: Int) {
+        calorieContent = calorie
+        super.init(beverage: beverage)
+    }
 }
 
-class Soda: Beverage {
+final class Coke: Soda, SugarCheckable {
     
-    private let taste: SodaTaste
-    private let calorie: Int
+    internal let sugarContent: Int
     
-    init(brand: String, capacity: Int, price: Int, name: String, calorie: Int, date: Date, taste: SodaTaste) {
-        self.taste = taste
-        self.calorie = calorie
-        super.init(brand: brand, capacity: capacity, price: price, name: name, date: date)
+    init(brand: String, capacity: Int, price: Int, name: String, date: Date, calorie: Int, sugar: Int) {
+        sugarContent = sugar
+        super.init(brand: brand, capacity: capacity, price: price, name: name, date: date, calorie: calorie)
+    }
+    
+    init(beverage: Beverage, calorie: Int, sugar: Int) {
+        sugarContent = sugar
+        super.init(beverage: beverage, calorie: calorie)
+    }
+}
+
+
+final class Cider: Soda, SugarCheckable {
+    
+    internal let sugarContent: Int
+
+    init(brand: String, capacity: Int, price: Int, name: String, date: Date, calorie: Int, sugar: Int) {
+        sugarContent = sugar
+        super.init(brand: brand, capacity: capacity, price: price, name: name, date: date, calorie: calorie)
+    }
+    
+    init(beverage: Beverage, calorie: Int, sugar: Int) {
+        sugarContent = sugar
+        super.init(beverage: beverage, calorie: calorie)
     }
 }
