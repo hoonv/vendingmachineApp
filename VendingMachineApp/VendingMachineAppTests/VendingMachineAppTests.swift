7c56cdb64320755c3b5bd98c203d23836523a3de
hoonv <chaeefe@gmail.com>
2020-09-14 12:52:45 +0900

    feat: init Project
    - 프로젝트 생성
    - 프로젝트 아이패드용으로 설정
    - 스토리보드 화면 아이패드용으로 변경
    - 깃이그노어 추가

---
+++ VendingMachineApp/VendingMachineAppTests/VendingMachineAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  VendingMachineAppTests.swift
+//  VendingMachineAppTests
+//
+//  Created by 채훈기 on 2020/09/14.
+//  Copyright © 2020 채훈기. All rights reserved.
+//
+
+import XCTest
+@testable import VendingMachineApp
+
+class VendingMachineAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
