c33d2f2d7f4abe4196cd81c03a7802943eb0693a
hoonv <chaeefe@gmail.com>
2020-09-15 14:39:45 +0900

    feat: add factory && line of Beverage
    - 음료를 빠르게 만들기 위한 공장
    - 자판기에 한줄 한줄 을 나타내기위한 line

--- VendingMachineApp/VendingMachineApp/Model/BeverageFactory.swift
+++ VendingMachineApp/VendingMachineApp/Model/BeverageFactory.swift
@@ -10,33 +10,39 @@ import Foundation
 
 class BeverageFactory {
     
-    public func makeCoke() -> Beverage {
-        return Coke(brand: "코카콜라", capacity: 350, price: 2000,
-                    name: "캔코카콜라", date: Date(), calorie: 200, sugar: 50)
+    public func makeCoke(count: Int = 1) -> [Beverage] {
+        return (0..<count).map { _ in
+            Coke(brand: "코카콜라", capacity: 350, price: 2000,
+                 name: "캔코카콜라", date: Date(), calorie: 200, sugar: 50) }
     }
     
-    public func makeFanta() -> Beverage {
-        return Cider(brand: "칠성사이다", capacity: 350, price: 2000,
-                     name: "칠성사이다", date: Date(), calorie: 200, sugar: 50)
+    public func makeFanta(count: Int = 1) -> [Beverage] {
+        return  (0..<count).map { _ in
+            Cider(brand: "칠성사이다", capacity: 350, price: 2000,
+                  name: "칠성사이다", date: Date(), calorie: 200, sugar: 50) }
     }
     
-    public func makeCantata() -> Beverage {
-        return Cantata(brand: "롯데", capacity: 300, price: 2500,
-                       name: "칸타타", date: Date(), caffeine: 20, sugar: 5)
+    public func makeCantata(count: Int = 1) -> [Beverage] {
+        return (0..<count).map { _ in
+            Cantata(brand: "롯데", capacity: 300, price: 2500,
+                    name: "칸타타", date: Date(), caffeine: 20, sugar: 5) }
     }
     
-    public func makeGeorgia() -> Beverage {
-        return Georgia(brand: "코카콜라", capacity: 300, price: 2000,
-                       name: "조지아커피", date: Date(), caffeine: 30, milk: 50)
+    public func makeGeorgia(count: Int = 1) -> [Beverage] {
+        return (0..<count).map { _ in
+            Georgia(brand: "코카콜라", capacity: 300, price: 2000,
+                    name: "조지아커피", date: Date(), caffeine: 30, milk: 50) }
     }
     
-    public func makeChocoMilk() -> Beverage {
-        return ChocoMilk(brand: "서울우유", capacity: 250, price: 1500,
-                         name: "서울초코우유", date: Date(), milk: 200, sugar: 50)
+    public func makeChocoMilk(count: Int = 1) -> [Beverage] {
+        return (0..<count).map { _ in
+            ChocoMilk(brand: "서울우유", capacity: 250, price: 1500,
+                      name: "서울초코우유", date: Date(), milk: 200, sugar: 50) }
     }
     
-    public func makeStrawberryMilk() -> Beverage {
-        return StrawberryMilk(brand: "충남우유", capacity: 250, price: 1500,
-                              name: "논산딸기우유", date: Date(), milk: 200, fat: 20)
+    public func makeStrawberryMilk(count: Int = 1) -> [Beverage] {
+        return (0..<count).map { _ in
+            StrawberryMilk(brand: "충남우유", capacity: 250, price: 1500,
+                           name: "논산딸기우유", date: Date(), milk: 200, fat: 20) }
     }
 }
---
+++ VendingMachineApp/VendingMachineApp/Model/BeverageLine.swift
@@ -0,0 +1,47 @@
+//
+//  BeverageLine.swift
+//  VendingMachineApp
+//
+//  Created by 채훈기 on 2020/09/15.
+//  Copyright © 2020 채훈기. All rights reserved.
+//
+
+import Foundation
+
+class BeverageLine {
+    
+    private(set) var sample: Beverage
+    private var items: [Beverage]
+    var count: Int { return items.count }
+    
+    init(sample: Beverage, items: [Beverage] = []) {
+        self.sample = sample
+        self.items = items
+    }
+    
+    func isEmpty() -> Bool {
+        return items.isEmpty
+    }
+    
+    func isEqualToSample(another: Beverage) -> Bool {
+        return sample == another
+    }
+    
+    func canBuy(coin: Int) -> Bool{
+        return coin >= sample.price && !isEmpty()
+    }
+    
+    func popFirst() -> Beverage? {
+        if isEmpty() { return nil }
+        return items.removeFirst()
+    }
+    
+    func popLast() -> Beverage? {
+        return items.popLast()
+    }
+    
+    func append(new: Beverage) {
+        guard new == sample else { return }
+        items.append(new)
+    }
+}
