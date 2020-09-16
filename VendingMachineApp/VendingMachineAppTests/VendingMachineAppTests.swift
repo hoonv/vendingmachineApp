8a02e62aaf234cb64f9b904780f26647be6c6bf8
hoonv <chaeefe@gmail.com>
2020-09-16 20:26:54 +0900

    feat: update test
    - 코드 수정으로 인한 테스트코드 변경

--- VendingMachineApp/VendingMachineAppTests/VendingMachineAppTests.swift
+++ VendingMachineApp/VendingMachineAppTests/VendingMachineAppTests.swift
@@ -15,9 +15,9 @@ class VendingMachineAppTests: XCTestCase {
     var factory = BeverageFactory()
     
     override func setUpWithError() throws {
-        vm.addStockBeverages(items: factory.makeBeverages(kind: .cantata, count: 10)) // 2500원
-        vm.addStockBeverages(items: factory.makeBeverages(kind: .coke, count: 10)) // 2000원
-        vm.addStockBeverages(items: factory.makeBeverages(kind: .chocoMilk, count: 10)) // 1500원
+        vm.addProducts(beverages: factory.makeBeverages(kind: .cantata, count: 10))
+        vm.addProducts(beverages: factory.makeBeverages(kind: .coke, count: 10))
+        vm.addProducts(beverages: factory.makeBeverages(kind: .chocoMilk, count: 10))
     }
 
     override func tearDownWithError() throws {
@@ -25,45 +25,45 @@ class VendingMachineAppTests: XCTestCase {
     }
 
     func test_1500원으로_살수있는_초코우유() throws {
-        vm.pushCoin(of: 1500)
-        XCTAssertEqual(vm.canBuy().count, 1)
+        vm.receiveBalance(coin: 1500)
+        XCTAssertEqual(vm.isAvailableProductsToSell().count, 1)
     }
 
     func test_2000원으로_살수있는_초코우유와콜라() throws {
-        vm.pushCoin(of: 2000)
-        XCTAssertEqual(vm.canBuy().count, 2)
+        vm.receiveBalance(coin: 2000)
+        XCTAssertEqual(vm.isAvailableProductsToSell().count, 2)
     }
 
     func test_2500원으로_살수있는_초코우유와콜라칸타타() throws {
-        vm.pushCoin(of: 2500)
-        XCTAssertEqual(vm.canBuy().count, 3)
+        vm.receiveBalance(coin: 2500)
+        XCTAssertEqual(vm.isAvailableProductsToSell().count, 3)
     }
     
     func test_1500원투입후_초코우유사먹고_0원확인() throws {
-        vm.pushCoin(of: 1500)
-        let _ = vm.buyBeverage(item: factory.makeBeverage(kind: .chocoMilk))
-        XCTAssertEqual(vm.currentCoin(), 0)
+        vm.receiveBalance(coin: 1500)
+        let _ = vm.receiveOrder(index: 2)
+        XCTAssertEqual(vm.currentBalance(), 0)
     }
 
     func test_1500원투입후_콜라사먹으려하지만_실패해서_돈그대로() throws {
-        vm.pushCoin(of: 1500)
-        let _ = vm.buyBeverage(item: factory.makeBeverage(kind: .coke))
-        XCTAssertEqual(vm.currentCoin(), 1500)
+        vm.receiveBalance(coin: 1500)
+        let _ = vm.receiveOrder(index: 1)
+        XCTAssertEqual(vm.currentBalance(), 1500)
     }
     
     func test_5000원투입후_칸타타콜라사먹고_남은돈500원() throws {
-        vm.pushCoin(of: 5000)
-        let _ = vm.buyBeverage(item: factory.makeBeverage(kind: .cantata))
-        let _ = vm.buyBeverage(item: factory.makeBeverage(kind: .coke))
-        XCTAssertEqual(vm.currentCoin(), 500)
+        vm.receiveBalance(coin: 5000)
+        let _ = vm.receiveOrder(index: 0)
+        let _ = vm.receiveOrder(index: 1)
+        XCTAssertEqual(vm.currentBalance(), 500)
     }
     
     func test_5000원투입후_칸타타콜라사먹고_남은품목28개() throws {
-        vm.pushCoin(of: 5000)
-        let _ = vm.buyBeverage(item: factory.makeBeverage(kind: .coke))
-        let _ = vm.buyBeverage(item: factory.makeBeverage(kind: .cantata))
-        let count = vm.stockToDictionary().reduce(0) {
-            $0 + $1.value
+        vm.receiveBalance(coin: 5000)
+        let _ = vm.receiveOrder(index: 1)
+        let _ = vm.receiveOrder(index: 0)
+        let count = vm.productState().reduce(0) {
+            $0 + $1.1
         }
         XCTAssertEqual(count, 28)
     }
