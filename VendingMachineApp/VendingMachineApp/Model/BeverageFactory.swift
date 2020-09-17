765859b52a88e4cf840b951733d305eff97cb355
hoonv <chaeefe@gmail.com>
2020-09-17 16:44:31 +0900

    feat: factory to static
    ##### 제목은 최대 50 글자까지만 입력 ############## -> |
    
    
    # 본문은 위에 작성
    ######## 본문은 한 줄에 최대 72 글자까지만 입력 ########################### -> |
    
    # 꼬릿말은 아래에 작성: ex) #이슈 번호
    
    # --- COMMIT END ---
    # <타입> 리스트
    #   feat    : 기능 (새로운 기능)
    #   fix     : 버그 (버그 수정)
    #   refactor: 리팩토링
    #   style   : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs    : 문서 (문서 추가, 수정, 삭제)
    #   test    : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore   : 기타 변경사항 (빌드 스크립트 수정 등)
    # ------------------
    #     제목 첫 글자를 대문자로
    #     제목은 명령문으로
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    #     본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------

--- VendingMachineApp/VendingMachineApp/Model/BeverageFactory.swift
+++ VendingMachineApp/VendingMachineApp/Model/BeverageFactory.swift
@@ -14,14 +14,14 @@ class BeverageFactory {
         case coke, cider, cantata, georgia, chocoMilk, strawberryMilk
     }
     
-    public func makeBeverage(kind: BeverageKind) -> Beverage {
+    static public func makeBeverage(kind: BeverageKind) -> Beverage {
         switch kind {
         case .coke:
             return Coke(brand: "코카콜라", capacity: 350, price: 2000,
                         name: "캔코카콜라", date: Date(), calorie: 200, sugar: 50)
         case .cantata:
             return Cantata(brand: "롯데", capacity: 300, price: 2500,
-                           name: "칸타타", date: Date(), caffeine: 20, sugar: 5)
+                           name: "칸타타커피", date: Date(), caffeine: 20, sugar: 5)
         case .chocoMilk:
             return ChocoMilk(brand: "서울우유", capacity: 250, price: 1500,
                              name: "서울초코우유", date: Date(), milk: 200, sugar: 50)
@@ -37,7 +37,7 @@ class BeverageFactory {
         }
     }
     
-    public func makeBeverages(kind: BeverageKind, count: Int) -> [Beverage] {
+    static public func makeBeverages(kind: BeverageKind, count: Int) -> [Beverage] {
         guard count > 0 else { return [] }
         return (0..<count).map{ _ in makeBeverage(kind: kind)}
     }
