19bcdece4e9751668abfd20bef85947ea167a54e
hoonv <chaeefe@gmail.com>
2020-09-24 17:06:34 +0900

    feat: create camera view controller
    - 카메라  뷰 컨트롤러 생성

---
+++ VendingMachineApp/VendingMachineApp/Controller/CameraViewController.swift
@@ -0,0 +1,208 @@
+//
+//  CameraViewController.swift
+//  VendingMachineApp
+//
+//  Created by 채훈기 on 2020/09/23.
+//  Copyright © 2020 채훈기. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraViewController: UIViewController {
+
+    @IBOutlet weak var takePicture: UIButton!
+    
+    var videoOutput : AVCaptureVideoDataOutput!
+    var captureSession : AVCaptureSession!
+    var backCamera : AVCaptureDevice!
+    var frontCamera : AVCaptureDevice!
+    var backInput : AVCaptureInput!
+    var frontInput : AVCaptureInput!
+    var previewLayer : AVCaptureVideoPreviewLayer!
+    var takePictureBool = false
+    let capturedImageView = CapturedImageView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(capturedImageView)
+        
+        takePicture.translatesAutoresizingMaskIntoConstraints = false
+        capturedImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            takePicture.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            takePicture.heightAnchor.constraint(equalToConstant: 50),
+            takePicture.widthAnchor.constraint(equalToConstant: 50),
+            takePicture.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            capturedImageView.heightAnchor.constraint(equalToConstant: 55),
+            capturedImageView.widthAnchor.constraint(equalToConstant: 55),
+            capturedImageView.topAnchor.constraint(equalTo: takePicture.bottomAnchor, constant: 30),
+            capturedImageView.trailingAnchor.constraint(equalTo: takePicture.trailingAnchor)
+
+        ])
+        takePicture.layer.cornerRadius = 25
+        capturedImageView.layer.cornerRadius = 10
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        checkPermissions()
+        setupAndStartCaptureSession()
+    }
+
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+
+        if let videoPreviewLayerConnection = previewLayer.connection {
+            let deviceOrientation = UIDevice.current.orientation
+            guard let newVideoOrientation = AVCaptureVideoOrientation(rawValue: deviceOrientation.rawValue),
+                deviceOrientation.isPortrait || deviceOrientation.isLandscape else {
+                    return
+            }
+
+            videoPreviewLayerConnection.videoOrientation = newVideoOrientation
+            videoOutput.connections.first?.videoOrientation = newVideoOrientation
+        }
+    }
+    
+    @IBAction func didPressTakePhoto(_ sender: UIButton) {
+        takePictureBool = true
+    }
+    
+    func setupAndStartCaptureSession(){
+        DispatchQueue.global(qos: .userInitiated).async{
+            //init session
+            self.captureSession = AVCaptureSession()
+            //start configuration
+            self.captureSession.beginConfiguration()
+
+            //session specific configuration
+            //before setting a session presets, we should check if the session supports it
+            if self.captureSession.canSetSessionPreset(.photo) {
+                self.captureSession.sessionPreset = .photo
+            }
+            self.captureSession.automaticallyConfiguresCaptureDeviceForWideColor = true
+
+            self.setupInputs()
+            
+            DispatchQueue.main.async {
+                //setup preview layer
+                self.setupPreviewLayer()
+            }
+            self.setupOutput()
+
+            //commit configuration
+            self.captureSession.commitConfiguration()
+            //start running it
+            self.captureSession.startRunning()
+        }
+    }
+    
+    func setupOutput(){
+        videoOutput = AVCaptureVideoDataOutput()
+        let videoQueue = DispatchQueue(label: "videoQueue", qos: .userInteractive)
+        videoOutput.setSampleBufferDelegate(self, queue: videoQueue)
+
+        if captureSession.canAddOutput(videoOutput) {
+            captureSession.addOutput(videoOutput)
+        } else {
+            fatalError("could not add video output")
+        }
+        videoOutput.connections.first?.videoOrientation = .portrait
+    }
+    
+    func setupPreviewLayer(){
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        view.layer.insertSublayer(previewLayer, below: takePicture.layer)
+        previewLayer.frame = self.view.layer.frame
+        previewLayer.videoGravity = .resizeAspectFill
+    }
+    
+    func setupInputs(){
+        //get back camera
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            backCamera = device
+        } else {
+            //handle this appropriately for production purposes
+            fatalError("no back camera")
+        }
+        //get front camera
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) {
+            frontCamera = device
+        } else {
+            fatalError("no front camera")
+        }
+        
+        //now we need to create an input objects from our devices
+        guard let bInput = try? AVCaptureDeviceInput(device: backCamera) else {
+            fatalError("could not create input device from back camera")
+        }
+        backInput = bInput
+        if !captureSession.canAddInput(backInput) {
+            fatalError("could not add back camera input to capture session")
+        }
+        
+        guard let fInput = try? AVCaptureDeviceInput(device: frontCamera) else {
+            fatalError("could not create input device from front camera")
+        }
+        frontInput = fInput
+        if !captureSession.canAddInput(frontInput) {
+            fatalError("could not add front camera input to capture session")
+        }
+        
+        //connect back camera input to session
+        captureSession.addInput(backInput)
+    }
+    
+    func checkPermissions() {
+        let cameraAuthStatus =  AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
+        switch cameraAuthStatus {
+          case .authorized:
+            return
+          case .denied:
+            abort()
+          case .notDetermined:
+            AVCaptureDevice.requestAccess(for: AVMediaType.video, completionHandler:
+            { (authorized) in
+              if(!authorized){
+                abort()
+              }
+            })
+          case .restricted:
+            abort()
+          @unknown default:
+            fatalError()
+        }
+    }
+}
+
+extension CameraViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        if !takePictureBool {
+            return //we have nothing to do with the image buffer
+        }
+        
+        //try and get a CVImageBuffer out of the sample buffer
+        guard let cvBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        
+        //get a CIImage out of the CVImageBuffer
+        let ciImage = CIImage(cvImageBuffer: cvBuffer)
+        
+        //get UIImage out of CIImage
+        let uiImage = UIImage(ciImage: ciImage)
+        
+        DispatchQueue.main.async {
+            self.capturedImageView.image = uiImage
+            self.takePictureBool = false
+        }
+    }
+}
